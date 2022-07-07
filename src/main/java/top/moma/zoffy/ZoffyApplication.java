291697cb7eb97ae5f45b941bbdf4f83c65536dae
jipeng.dong <jipeng.dong@opay-inc.com>
2022-07-07 15:30:58 +0800

    init

---
+++ src/main/java/top/moma/zoffy/ZoffyApplication.java
@@ -0,0 +1,12 @@
+package top.moma.zoffy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZoffyApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ZoffyApplication.class, args);
+  }
+}
---
+++ src/test/java/top/moma/zoffy/ZoffyApplicationTests.java
@@ -0,0 +1,11 @@
+package top.moma.zoffy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZoffyApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
